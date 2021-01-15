7e68f82d40ce77ccf2e6e639e703594f66a11f5a
Andrew Stanish <andybp85@gmail.com>
2021-01-14 23:02:36 -0500

    notes working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from "@angular/core"
+import { AfterViewInit, Component } from "@angular/core"
+import Vex from "vexflow"
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = "andys-music-tutor"
+export class AppComponent implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+    const vf = new Vex.Flow.Factory({
+      renderer: {elementId: "notes", width: 500, height: 200}
+    })
+
+    const score = vf.EasyScore()
+    const system = vf.System()
+
+    system.addStave({
+      voices: [
+        score.voice(score.notes("C#5/q, B4, A4, G#4", {stem: "up"}), {}),
+        score.voice(score.notes("C#4/h, C#4", {stem: "down"}), {})
+      ]
+    }).addClef("treble").addTimeSignature("4/4")
+
+    vf.draw()
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
+import { NgModule } from "@angular/core"
 import { MatCardModule } from "@angular/material/card"
 import { BrowserModule } from "@angular/platform-browser"
-import { NgModule } from "@angular/core"
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
 
 import { AppComponent } from "./app.component"
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
 
 @NgModule({
   declarations: [
