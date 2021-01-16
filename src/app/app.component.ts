521af77d672d14ed81062c9bf17754568fb347fc
Andrew Stanish <andybp85@gmail.com>
2021-01-16 11:19:19 -0500

    2 space indenting sucks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,76 @@
-import { AfterViewInit, Component } from "@angular/core"
+import { AfterViewInit, Component, ElementRef, ViewChild } from "@angular/core"
+import { range, sample } from "lodash-es"
 import Vex from "vexflow"
 
+const VF = Vex.Flow
+
+const noteInfo = [
+    {
+        note: "a",
+        octaves: [2, 3]
+    }, {
+        note: "b",
+        octaves: [2, 3]
+    }, {
+        note: "c",
+        octaves: [3, 4]
+    }, {
+        note: "d",
+        octaves: [3]
+    }, {
+        note: "e",
+        octaves: [2, 3]
+    }, {
+        note: "f",
+        octaves: [2, 3]
+    }, {
+        note: "g",
+        octaves: [2, 3]
+    }
+]
+
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.scss"]
 })
 export class AppComponent implements AfterViewInit {
 
-  ngAfterViewInit(): void {
-    const vf = new Vex.Flow.Factory({
-      renderer: {elementId: "notes", width: 500, height: 200}
-    })
+    @ViewChild("notes") notesRef!: ElementRef<HTMLDivElement>
 
-    const score = vf.EasyScore()
-    const system = vf.System()
+    timer = 1
+    notes = []
 
-    system.addStave({
-      voices: [
-        score.voice(score.notes("C#5/q, B4, A4, G#4", {stem: "up"}), {}),
-        score.voice(score.notes("C#4/h, C#4", {stem: "down"}), {})
-      ]
-    }).addClef("treble").addTimeSignature("4/4")
+    ngAfterViewInit(): void {
+        const renderer = new VF.Renderer(this.notesRef.nativeElement, VF.Renderer.Backends.SVG)
+        renderer.resize(500, 200)
+        const context = renderer.getContext()
+        const stave = new VF.Stave(10, 40, 150)
+        stave.addClef("bass")
+        stave.setContext(context).draw()
 
-    vf.draw()
-  }
+        const notes = range(0, 2).reduce(acc => {
+
+            const aNote = sample(noteInfo)
+            const mod = sample(["b", "", "#"])
+
+            const note = new VF.StaveNote({
+                clef: "bass",
+                // tslint:disable-next-line:no-non-null-assertion
+                keys: [`${aNote!.note}${mod}/${sample(aNote!.octaves)}`],
+                // tslint:disable-next-line:no-non-null-assertion
+                duration: sample(["4", "8"])!
+            })
+
+            return [...acc, mod === ""
+                ? note
+                // tslint:disable-next-line:no-non-null-assertion
+                : note.addAccidental(0, new VF.Accidental(mod!))]
+        }, [] as Vex.Flow.StaveNote[])
+
+        const beams = VF.Beam.generateBeams(notes)
+        VF.Formatter.FormatAndDraw(context, stave, notes)
+        beams.forEach(b => b.setContext(context).draw())
+
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,15 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
 import { AppComponent } from "./app.component"
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatCardModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatCardModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
